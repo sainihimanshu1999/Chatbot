219f40747983a70e789b79d890e1d5d38950fb57
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-03-07 23:34:53 +0530

    initial commit

---
+++ chatbot.py
@@ -0,0 +1,10 @@
+import nltk
+nltk.download('punkt')
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import tensorflow as tf
+import numpy as np
+import tflear
+import random
+import jso
