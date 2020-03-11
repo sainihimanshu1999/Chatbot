de9547ff5f5b968c113a79253954f4849139eed1
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-03-11 21:57:27 +0530

    Fifth commit

--- chatbot.py
+++ chatbot.py
@@ -101,6 +101,71 @@ model.fit(train_x, train_y, n_epoch=1000, batch_size=8, show_metric=True)
 model.save('model.tflearn')
 
 
-#Impoting pickle
+#Importing pickle
 import pickle
 pickle.dump( {'words':words, 'classes':classes, 'train_x':train_x, 'train_y':train_y}, open( "training_data", "wb" ) )
+
+
+#restoring all the datastructures
+data = pickle.load(open("training_data", "rb"))
+words = data['words']
+classes = data['classes']
+train_x = data['train_x']
+train_y = data['train_y']
+
+with open('intents.json') as json_data:
+  intents = json.load(json_data)
+
+  #loading the saved model
+model.load('./model.tflearn')
+
+
+
+#training the model
+def clean_up_sentence(sentence):
+  sentence_words = nltk.word_tokenize(sentence)
+  sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+  return sentence_words
+
+
+def bow(sentence, words, show_details = False):
+  sentence_words = clean_up_sentence(sentence)
+  bag = [0]*len(words)
+  for s in sentence_words:
+    for i,w in enumerate(words):
+      if w == s:
+        bag[i] = 1
+        if show_details:
+          print('found in bag: %s' %w)
+    return(np.array(bag))
+
+
+
+
+    ERROR_THRESHOLD = 0.30
+def classify(sentence):
+   
+    results = model.predict([bow(sentence, words)])[0]
+   
+    results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD]
+   
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append((classes[r[0]], r[1]))
+    
+    return return_list
+
+def response(sentence, userID='123', show_details=False):
+    results = classify(sentence)
+    
+    if results:
+        
+        while results:
+            for i in intents['intents']:
+                
+                if i['tag'] == results[0][0]:
+                    
+                    return print(random.choice(i['responses']))
+
+            results.pop(0)
