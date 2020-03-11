ea1022bdefb70447ef6cc33c280ac3b2e26ae4fc
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-03-11 10:39:37 +0530

    Fourth Commit

--- chatbot.py
+++ chatbot.py
@@ -49,3 +49,58 @@ classes = sorted(list(set(classes)))
 print(len(documents) , "documents")
 print(len(classes) , "classes" , classes)
 print(len(words) , "unique stem words" , words)
+
+# create training data
+training = []
+output = []
+# create an empty array for output
+output_empty = [0] * len(classes)
+
+# create training set, bag of words for each sentence
+for doc in documents:
+    # initialize bag of words
+    bag = []
+    #tokenized words for the pattern
+    pattern_words = doc[0]
+    # stemming
+    pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
+    # create bag of words array
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+
+    # output is '1' for current tag and '0' for rest of other tags
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+
+    training.append([bag, output_row])
+
+# shuffling features and turning it into np.array
+random.shuffle(training)
+training = np.array(training)
+
+# creating training lists
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+
+
+# resetting underlying graph data
+tf.reset_default_graph()
+
+# Building neural network
+net = tflearn.input_data(shape=[None, len(train_x[0])])
+net = tflearn.fully_connected(net, 10)
+net = tflearn.fully_connected(net, 10)
+net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
+net = tflearn.regression(net)
+
+# Defining model and setting up tensorboard
+model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+
+# Start training
+model.fit(train_x, train_y, n_epoch=1000, batch_size=8, show_metric=True)
+model.save('model.tflearn')
+
+
+#Impoting pickle
+import pickle
+pickle.dump( {'words':words, 'classes':classes, 'train_x':train_x, 'train_y':train_y}, open( "training_data", "wb" ) )
